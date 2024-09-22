91681cb625d4da662ded8dc674665542520c6ae7
= <today6215@gmail.com>
2024-09-22 23:38:15 +0900

    first commit

---
+++ src/main/java/com/zerobase/communityproject/CommunityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.communityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/communityproject/CommunityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.communityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
