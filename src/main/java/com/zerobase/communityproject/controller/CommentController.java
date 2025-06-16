5616191ecdf6e9bd140f31c55764ba0432be94b2
= <today6215@gmail.com>
2025-06-16 20:25:59 +0900

    오타 수정

--- src/main/java/com/zerobase/communityproject/controller/CommentController.java
+++ src/main/java/com/zerobase/communityproject/controller/CommentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("comment")
+@RequestMapping("/comment")
 @RequiredArgsConstructor
 public class CommentController {
 
