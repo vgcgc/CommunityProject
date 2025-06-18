5af3f3df3a6812813697772ea95055a6ba539cab
= <today6215@gmail.com>
2025-06-18 14:23:34 +0900

    jpa @EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/zerobase/communityproject/CommunityProjectApplication.java
+++ src/main/java/com/zerobase/communityproject/CommunityProjectApplication.java
@@ -2,8 +2,10 @@ package com.zerobase.communityproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CommunityProjectApplication {
 
   public static void main(String[] args) {
