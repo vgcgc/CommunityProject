7afe21728d7c110f404a3281640bf742df2a595e
= <today6215@gmail.com>
2025-06-18 14:20:26 +0900

    공통 컬럼을 위한 엔티티 설계

---
+++ src/main/java/com/zerobase/communityproject/entity/Base.java
@@ -0,0 +1,45 @@
+package com.zerobase.communityproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Base {
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdDate;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime modifiedDate;
+
+  @Column(name = "deleted_at")
+  private LocalDateTime deletedDate;
+
+  @PrePersist
+  protected void prePersist() {
+    this.createdDate = LocalDateTime.now();
+    this.deletedDate = null;
+  }
+
+  @PreUpdate
+  protected void preUpdate() {
+    this.modifiedDate = LocalDateTime.now();
+  }
+
+  public void delete() {
+    this.deletedDate = LocalDateTime.now();
+  }
+
+}
